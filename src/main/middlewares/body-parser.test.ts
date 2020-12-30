41248a93abb53d8c62b5c71828e2e37afd4564aa
gideaopinheiro <gpss@ic.ufal.br>
2020-12-30 13:10:15 -0300

    test: ensure parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import app from '../config/app'
+import request from 'supertest'
+
+describe('BodyParser Middleware', () => {
+  test('', async () => {
+    app.post('/body_parser_test', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/body_parser_test')
+      .send({ name: 'any_name' })
+      .expect({ name: 'any_name' })
+  })
+})
