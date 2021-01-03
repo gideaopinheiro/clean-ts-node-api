236417b1406dd8108bb955f4912ce6dfe3ea0453
gideaopinheiro <gpss@ic.ufal.br>
2021-01-03 12:05:54 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,11 +1,9 @@
-import { InvalidParamError } from '../../errors'
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { Validation } from '../../helpers/validators/validation'
-import { AddAccount, Controller, EmailValidator, HttpRequest, HttpResponse } from '../signup/signup-protocols'
+import { AddAccount, Controller, HttpRequest, HttpResponse } from '../signup/signup-protocols'
 
 export class SignUpController implements Controller {
   constructor (
-    private readonly emailValidator: EmailValidator,
     private readonly addAccount: AddAccount,
     private readonly validation: Validation
   ) {}
@@ -17,10 +15,6 @@ export class SignUpController implements Controller {
         return badRequest(error)
       }
       const { name, email, password } = httpRequest.body
-      const isValid = this.emailValidator.isValid(email)
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'))
-      }
       const account = await this.addAccount.add({
         name,
         email,
