de8c176ad3785e021a4b94d28558d07caf86bbbb
gideaopinheiro <gpss@ic.ufal.br>
2021-01-09 10:00:05 -0300

    refactor: move validation to its own layer

--- src/main/factories/controllers/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login-validation-factory.spec.ts
@@ -1,8 +1,8 @@
-import { EmailValidation, ValidationComposite, RequiredFieldValidation } from '../../../../presentation/helpers/validators'
+import { EmailValidation, ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
 import { Validation, EmailValidator } from '../../../../presentation/protocols'
 import { makeLoginValidation } from './login-validation-factory'
 
-jest.mock('../../../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../../validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
