17aafddb8529e81224d14b9348b8b00637269e05
gideaopinheiro <gpss@ic.ufal.br>
2021-01-12 11:55:55 -0300

    test: ensure LoadSurveys routes returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -24,54 +24,64 @@ describe('Survey Routes', () => {
     await MongoHelper.disconnect()
   })
 
-  test('Should return 403 if no access token is provided', async () => {
-    await request(app)
-      .post('/api/surveys')
-      .send({
-        question: 'Question',
-        answers: [
-          {
-            answer: 'Answer 1',
-            image: 'http://image-name.com'
-          },
-          {
-            answer: 'Answer 2'
-          }
-        ]
-      }).expect(403)
+  describe('POST /surveys', () => {
+    test('Should return 403 if no access token is provided', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        }).expect(403)
+    })
+
+    test('Should return 204 with valid token', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Gideão',
+        email: 'gideao345@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      },
+      {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        }).expect(204)
+    })
   })
 
-  test('Should return 204 with valid token', async () => {
-    const res = await accountCollection.insertOne({
-      name: 'Gideão',
-      email: 'gideao345@gmail.com',
-      password: '123',
-      role: 'admin'
+  describe('POST /surveys', () => {
+    test('Should return 403 on load surveys if no access token is provided', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
     })
-    const id = res.ops[0]._id
-    const accessToken = sign({ id }, env.jwtSecret)
-    await accountCollection.updateOne({
-      _id: id
-    },
-    {
-      $set: {
-        accessToken
-      }
-    })
-    await request(app)
-      .post('/api/surveys')
-      .set('x-access-token', accessToken)
-      .send({
-        question: 'Question',
-        answers: [
-          {
-            answer: 'Answer 1',
-            image: 'http://image-name.com'
-          },
-          {
-            answer: 'Answer 2'
-          }
-        ]
-      }).expect(204)
   })
 })
