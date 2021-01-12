eec9b32b93684431af5fcd3a816d2c53b71c3071
gideaopinheiro <gpss@ic.ufal.br>
2021-01-12 09:24:44 -0300

    test: ensure LoadSurveysController returns 200 on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,11 +1,45 @@
 import { Controller, LoadSurveys, SurveyModel } from './load-survey-controller-protocols'
 import { LoadSurveysController } from './load-surveys-controller'
 import MockDate from 'mockdate'
+import { ok } from '../../../helpers/http/http-helper'
+
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [
+    {
+      id: 'any_id',
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'any_answer2'
+        }
+      ],
+      date: new Date()
+    },
+    {
+      id: 'any_id2',
+      question: 'any_question2',
+      answers: [
+        {
+          image: 'any_image2',
+          answer: 'any_answer2'
+        },
+        {
+          answer: 'any_answer22'
+        }
+      ],
+      date: new Date()
+    }
+  ]
+}
 
 const makeLoadSurveys = (): LoadSurveys => {
   class LoadSurveysStub implements LoadSurveys {
     async load (): Promise<SurveyModel[]> {
-      return Promise.resolve(null)
+      return Promise.resolve(makeFakeSurveys())
     }
   }
   return new LoadSurveysStub()
@@ -39,5 +73,11 @@ describe('LoadSurveys Controller', () => {
     const loadSpy = jest.spyOn(loadSurveysStub, 'load')
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(ok(makeFakeSurveys()))
   })
 })
