c6368cfbcaffaf6149020ea0012f509bdea56abe
gideaopinheiro <gpss@ic.ufal.br>
2021-01-12 11:55:37 -0300

    feat: ensure LoadSurveys routes returns 403 if no access token is provided

---
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -0,0 +1,9 @@
+import { LoadSurveysController } from '../../../../../presentation/controllers/survey/load-surveys/load-surveys-controller'
+import { Controller } from '../../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../../usecases/decorator/log-controller-decorator-factory'
+import { makeDbLoadSurveys } from '../../../usecases/survey/load-surveys/db-load-surveys-factory'
+
+export const makeLoadSurveysController = (): Controller => {
+  const loadSurveysController = new LoadSurveysController(makeDbLoadSurveys())
+  return makeLogControllerDecorator(loadSurveysController)
+}
