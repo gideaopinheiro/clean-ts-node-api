1fb99270ff3397828a3dcfc07c4c69be59f550f1
gideaopinheiro <gpss@ic.ufal.br>
2021-01-12 11:11:20 -0300

    test: ensure DbLoadSurveys returns a list of surveys on success

---
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -0,0 +1,82 @@
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
+import { DbLoadSurveys } from './db-load-surveys'
+import MockDate from 'mockdate'
+
+const makeFakeSurveys = (): SurveyModel[] => {
+  return [
+    {
+      id: 'any_id',
+      question: 'any_question',
+      answers: [
+        {
+          image: 'any_image',
+          answer: 'any_answer'
+        },
+        {
+          answer: 'any_answer2'
+        }
+      ],
+      date: new Date()
+    },
+    {
+      id: 'any_id2',
+      question: 'any_question2',
+      answers: [
+        {
+          image: 'any_image2',
+          answer: 'any_answer2'
+        },
+        {
+          answer: 'any_answer22'
+        }
+      ],
+      date: new Date()
+    }
+  ]
+}
+
+const makeLoadSurveysRepository = (): LoadSurveysRepository => {
+  class LoadSurveysRepositoryStub implements LoadSurveysRepository {
+    async load (): Promise<SurveyModel[]> {
+      return Promise.resolve(makeFakeSurveys())
+    }
+  }
+  return new LoadSurveysRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbLoadSurveys
+  loadSurveysRepositoryStub: LoadSurveysRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysRepositoryStub = makeLoadSurveysRepository()
+  const sut = new DbLoadSurveys(loadSurveysRepositoryStub)
+  return {
+    sut,
+    loadSurveysRepositoryStub
+  }
+}
+
+describe('DbLoadSurveys', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+  test('Should calls LoadSurveysRepository', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    const loadSpy = jest.spyOn(loadSurveysRepositoryStub, 'load')
+    await sut.load()
+    expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return a list of surveys on success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.load()
+    expect(surveys).toEqual(makeFakeSurveys())
+  })
+})
