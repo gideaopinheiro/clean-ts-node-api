70b85a39f3f923f959cda3c1b0705b1f601cc25c
gideaopinheiro <gpss@ic.ufal.br>
2021-01-13 10:19:27 -0300

    test: ensure DbLoadSurveyById returns a survey if LoadSurveyByIdRepository succeeds

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -58,4 +58,10 @@ describe('DbLoadSurveyById usecase', () => {
     await sut.loadById('any_id')
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
   })
+
+  test('Should return a survey on success', async () => {
+    const { sut } = makeSut()
+    const survey = await sut.loadById('any_id')
+    expect(survey).toEqual(makeFakeSurvey())
+  })
 })
