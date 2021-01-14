cb6d8d56dfb34685f3b37a6b0a509b9991a3c5f8
gideaopinheiro <gpss@ic.ufal.br>
2021-01-14 14:42:17 -0300

    feat: ensure save survey result returns 403 if no token is provided

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -134,6 +134,7 @@ describe('SaveSurveyResult Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
   })
+
   test('Should return 200 on success', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle(makeFakeRequest())
