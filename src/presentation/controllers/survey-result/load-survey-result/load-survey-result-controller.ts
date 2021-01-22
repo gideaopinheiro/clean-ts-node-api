6cfc2051bd6d3ec17f43257b13cd91bca613f6c2
gideaopinheiro <gpss@ic.ufal.br>
2021-01-22 13:01:03 -0300

    feat: ensure LoasSurveyResultController calls LoadSurveyResult with correct value

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -2,17 +2,23 @@ import { LoadSurveyById } from '@/domain/usecases/survey/load-surveys-by-id'
 import { InvalidParamError } from '@/presentation/errors'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
 
 export class LoadSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly loadSurveyResult: LoadSurveyResult
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      const { surveyId } = httpRequest.params
+      const survey = await this.loadSurveyById.loadById(surveyId)
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
-      return Promise.resolve(null)
+      await this.loadSurveyResult.load(surveyId)
+      return null
     } catch (error) {
       return serverError(error)
     }
--- src/presentation/test/mock-survey-result.ts
+++ src/presentation/test/mock-survey-result.ts
@@ -1,6 +1,17 @@
 import { SurveyModel } from '@/domain/models/survey'
+import { mockSurvey, mockSurveyResult } from '@/domain/test'
 import { LoadSurveyById } from '@/domain/usecases/survey/load-surveys-by-id'
-import { mockSurvey } from '@/domain/test'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub implements LoadSurveyResult {
+    async load (surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResult())
+    }
+  }
+  return new LoadSurveyResultStub()
+}
 
 export const mockLoadSurveyById = (): LoadSurveyById => {
   class LoadSurveyByIdStub implements LoadSurveyById {
